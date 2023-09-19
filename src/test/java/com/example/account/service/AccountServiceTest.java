78d41b0e29b92c108fe27e34c8d18d6d860383f3
soeun135 <soensuckun@naver.com>
2023-09-20 02:15:14 +0900

    Mockito test

--- src/test/java/com/example/account/service/AccountServiceTest.java
+++ src/test/java/com/example/account/service/AccountServiceTest.java
@@ -2,28 +2,56 @@ package com.example.account.service;
 
 import com.example.account.domain.Account;
 import com.example.account.domain.AccountStatus;
+import com.example.account.repository.AccountRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class) //Mokito 확장 팩을 클래스에 달아줌
 class AccountServiceTest {
-    @Autowired
+
+    @Mock //서비스가 의존하는 부분을 가짜로 만들어서 넣어줌
+    private AccountRepository accountRepository;
+
+    @InjectMocks //Mock으로 가짜로 만들어준 리포지토리를 service에 주입
     private AccountService accountService;
-    @BeforeEach
-    void init() {
-        accountService.createAccount();
+    //BeforeEach가 필요업서짐
+
+    @Test
+    @DisplayName("계좌 조회 성공")
+    void testXXX() {
+        //given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("65789").build()));
+        //when
+        Account account = accountService.getAccount(4554535L);
+
+        //then
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
     }
+
     @Test
     void testGetAccount() {
         Account account = accountService.getAccount(1L);
 
         assertEquals("40000", account.getAccountNumber());
         assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
-     }
+    }
 
     @Test
     void testGetAccount2() {
