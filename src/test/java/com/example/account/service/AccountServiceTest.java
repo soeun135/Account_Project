967fd0b9e08c7a59e233e63a0c619217ad2e1e03
soeun135 <soensuckun@naver.com>
2023-09-20 00:55:29 +0900

    Junit test

---
+++ src/test/java/com/example/account/service/AccountServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.account.service;
+
+import com.example.account.domain.Account;
+import com.example.account.domain.AccountStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AccountServiceTest {
+    @Autowired
+    private AccountService accountService;
+    @BeforeEach
+    void init() {
+        accountService.createAccount();
+    }
+    @Test
+    void testGetAccount() {
+        Account account = accountService.getAccount(1L);
+
+        assertEquals("40000", account.getAccountNumber());
+        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
+     }
+
+    @Test
+    void testGetAccount2() {
+        Account account = accountService.getAccount(2L);
+
+        assertEquals("40000", account.getAccountNumber());
+        assertEquals(AccountStatus.IN_USE, account.getAccountStatus());
+    }
+}
