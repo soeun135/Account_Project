cb445b72e607d9dc217062833dfc8e257a22e3b5
soeun135 <soensuckun@naver.com>
2023-09-21 02:41:43 +0900

    계좌 해지 API 생성

--- src/main/java/com/example/account/service/AccountService.java
+++ src/main/java/com/example/account/service/AccountService.java
@@ -6,6 +6,7 @@ import com.example.account.dto.AccountDto;
 import com.example.account.exception.AccountException;
 import com.example.account.repository.AccountRepository;
 import com.example.account.repository.AccountUserRepository;
+import com.example.account.type.AccountStatus;
 import com.example.account.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,8 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Optional;
 
 import static com.example.account.type.AccountStatus.*;
 
@@ -61,5 +64,38 @@ public class AccountService {
             throw new RuntimeException("Minus");
         }
         return accountRepository.findById(id).get();
+    }
+
+    @Transactional
+    public AccountDto deleteAccount(Long userId, String accountNumber) {
+        //사용자와 계좌가 없는 경우
+        AccountUser accountUser = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+        Account account = accountRepository.findByAccountNumber(accountNumber)
+                .orElseThrow(() -> new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+        //사용자 아이디와 계좌 소유주가 다른 경우
+        //계좌가 이미 해지상태인 경우
+        //잔액이 있는 경우 싹 실패응답
+        validateDeleteAccount(accountUser, account);
+
+        account.setAccountStatus(UNREGISTERED);
+        account.setUnregisteredAt(LocalDateTime.now());
+
+        return AccountDto.fromEntity(account);
+    }
+
+    private void validateDeleteAccount(AccountUser accountUser, Account account) {
+        if(!Objects.equals(accountUser.getId(), account.getAccountUser().getId())) {
+            throw new AccountException(ErrorCode.USER_ACCOUNT_UN_MATCH);
+        }
+
+        if(account.getAccountStatus() == UNREGISTERED) {
+            throw new AccountException(ErrorCode.ACCOUNT_ALREADY_UNREGISTERED);
+        }
+
+        if(account.getBalance() > 0) {
+            throw new AccountException(ErrorCode.BALANCE_NOT_EMPTY);
+        }
     }
 }
--- src/main/java/com/example/account/type/ErrorCode.java
+++ src/main/java/com/example/account/type/ErrorCode.java
@@ -7,7 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     USER_NOT_FOUND("사용자가 없습니다."),
-    MAX_ACCOUNT_PER_USER_10("사용자 최대 계좌는 10개입니다.");
+    MAX_ACCOUNT_PER_USER_10("사용자 최대 계좌는 10개입니다."),
+    ACCOUNT_NOT_FOUND("계좌가 없습니다."),
+    USER_ACCOUNT_UN_MATCH("사용자와 계좌의 소유주가 다릅니다."),
+    ACCOUNT_ALREADY_UNREGISTERED("이미 해지된 계좌입니다."),
+    BALANCE_NOT_EMPTY("잔액이 있는 계좌 해지 불가");
 
     private final String description;
 
